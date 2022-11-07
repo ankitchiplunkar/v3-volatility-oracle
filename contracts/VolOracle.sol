c86e270e284fc7218ec504540bd1d0d26e09aea3
notjojo <1611805+PangZhi@users.noreply.github.com>
2022-11-07 15:01:07 -0800

    feat: add functions to calculate volatility

--- contracts/VolOracle.sol
+++ contracts/VolOracle.sol
@@ -157,4 +157,67 @@ contract VolOracle {
     function getObservation(address _pool, uint256 _idx) public view returns (VolObservation memory) {
         return oracleStates[_pool].observations[_idx];
     }
+
+
+    // @dev get the standdevation given specified days range from now
+    function getVol(address _pool, uint32 _daysToNow) public view returns(uint256 standardDeviation) {
+        uint32 target = uint32(block.timestamp) - _daysToNow * uint32(1 days);
+        // binary search to start index.
+        uint256 startIndex = getObservationIndexBeforeOrAtTarget(_pool, target);
+        // TODO: edge case - 1st element of the array
+
+        return calculateVol(_pool, startIndex);
+    }
+
+    // TODO: WIP
+    // @dev calculate the standdevation from startIndex to endIndex
+    function calculateVol(address _pool, uint256 startIndex) internal view returns(uint256 stanDeviation) {
+        VolOracleState memory volOracleState = oracleStates[_pool];
+        uint256 endIndex = volOracleState.observationIndex;
+        VolObservation memory startObservation = volOracleState.observations[startIndex];
+        VolObservation memory endObservation = volOracleState.observations[endIndex];
+        uint256 tickSquareSum = endObservation.tickSquareCumulative - startObservation.tickSquareCumulative;
+        uint256 timeEclapsed = uint256(endObservation.blockTimestamp - startObservation.blockTimestamp);
+        uint256 tickAvg = uint256(uint56(endObservation.tickCumulative - startObservation.tickCumulative)) / timeEclapsed;
+        uint256 stddev = (tickSquareSum - tickAvg * tickAvg * timeEclapsed) / timeEclapsed;
+        return stddev;
+    }
+
+    // @dev get the obesrvation index right before or at the target timestamp, using binary search
+    function getObservationIndexBeforeOrAtTarget(
+        address _pool,
+        uint32 _target
+    ) private view returns(uint256 observationIndexAfterTarget) {
+        VolOracleState memory volOracleState = oracleStates[_pool];
+        uint256 l = (volOracleState.observationIndex + 1) % OBSERVATION_SIZE;
+        uint256 r = l + OBSERVATION_SIZE - 1;
+        uint256 idx;
+        while (true) {
+            idx = (l + r) / 2;
+
+            VolObservation memory curObservation = volOracleState.observations[idx % OBSERVATION_SIZE];
+            if (curObservation.blockTimestamp == 0) {
+                // hasn't been initialized
+                l = idx + 1;
+                continue;
+            }
+
+            if (l == r) break;
+            if (curObservation.blockTimestamp <= _target) {
+                l = idx + 1;
+            } else {
+                r = idx - 1;
+            }
+        }
+
+        l = l % OBSERVATION_SIZE;
+        VolObservation memory observation = volOracleState.observations[l];
+        if (observation.blockTimestamp <= _target) {
+            return l;
+        } else {
+            return (l - 1 + OBSERVATION_SIZE) % OBSERVATION_SIZE;
+        }
+    }
+
+
 }
