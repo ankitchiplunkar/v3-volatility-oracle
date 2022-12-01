0b548aa0efeaa91f404238b18ffaa20914827cb6
notjojo <1611805+PangZhi@users.noreply.github.com>
2022-12-01 13:52:36 -0800

    chore: add gas reporter and deployment script

---
+++ scripts/deploy.ts
@@ -0,0 +1,17 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const maxFill = 10;
+
+  const VolOracle = await ethers.getContractFactory("VolOracle");
+  const volOracle = await VolOracle.deploy(maxFill);
+
+  await volOracle.deployed();
+
+  console.log(`Deployed VolOracle with maxFill ${maxFill} to address ${volOracle.address}`);
+}
+
+main().catch(error => {
+  console.error(error);
+  process.exitCode = 1;
+});
